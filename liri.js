d7f52349072efcacc7d311987c55fd95456f9be6
estherestherv <32546768+estherestherv@users.noreply.github.com>
2019-04-04 16:32:01 -0400

    j

--- liri.js
+++ liri.js
@@ -40,8 +40,20 @@ function concert_this() {
 }
 //    * `spotify-this-song`   
 function spotify_this_song() {
+    var songName = "What's My Age Again";// by blink-82
 
-}
+    spotify.search({type: "track ", query:songName}, function (error,data){
+        if(error){
+            return console.log("There is error  "+ error );
+        }
+        console.log(" ************Spotify Response **************");
+        console.log("Artist Name: "+data.tracks.items[0].album.artist[0].name);
+        console.log("Song Name: "+data.tracks.items[0].name);
+        console.log("Song Demo Link: "+data.tracks.items[0].href);
+        console.log("Song Name: "+data.tracks.items[0].album.name);
+    })
+};
+
 //    * `movie-this`
 function movie_this() {
     if (!movie) {
@@ -49,35 +61,28 @@ function movie_this() {
     }
 var movieQueryUrl = "http:www.omcbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-axios.request(movieQueryUrl).then{
-function(response){
-    console.log(" ************Movie Response **************");
-    console.log(" Movie Title: " +response.data.Title);
-    console.log(" Release Date: " +response.data.Year);
-    console.log(" IMDB Rating: " +response.data.Rating);
-    console.log(" Country Produced: " +response.data.Country);
-    console.log(" Language: " +response.data.Language);
-    console.log(" Plot: " +response.data.Plot);
-    console.log(" List of Actors: " +response.data.Actors);
-}
-}
-}
+// axios.request(movieQueryUrl).then{
+//     function(response){
+//         console.log(" ************Movie Response **************");
+//         console.log(" Movie Title: " +response.data.Title);
+//         console.log(" Release Date: " +response.data.Year);
+//         console.log(" IMDB Rating: " +response.data.Rating);
+//         console.log(" Country Produced: " +response.data.Country);
+//         console.log(" Language: " +response.data.Language);
+//         console.log(" Plot: " +response.data.Plot);
+//         console.log(" List of Actors: " +response.data.Actors);
+
+//         var logMovie = "********New Movie Entry***********" +response.data.Title;
+
+//         fs.appendFile("log.txt",logMovie,function (error){
+//             if(error) return (error);
+//         });
+//     }
+// };
+// }
 //    * `do-what-it-says`
 function do_what_it_says() {
 
 }
 
 
-// random text part
-function random() {
-    fs.readFile('random.txt', 'utf8', function (error, data) {
-        var dataArr = data.split(',');
-
-        if (error) {
-            return console.log(error);
-        } else {
-            console.log(data);
-
-        }
-    });
-};
